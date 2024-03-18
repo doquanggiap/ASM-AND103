53fc61b7826eb2624ebeed80f9fc55bc732aee60
doquanggiap <giapdqph34273@fpt.edu.vn>
2024-03-18 20:52:12 +0700

    ✅✅✅ Đăng ký, Đăng nhập

--- app/src/main/java/fpoly/giapdqph34273/asm_ph34273_md18306/Login.java
+++ app/src/main/java/fpoly/giapdqph34273/asm_ph34273_md18306/Login.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     TextInputEditText txtUser, txtPass;
@@ -21,6 +28,18 @@ public class Login extends AppCompatActivity {
 
     Button btndangnhap;
     TextView txtSignUp, txtResetPass;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            txtUser.setText(currentUser.getEmail());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +51,10 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         anhxa();
+        mAuth = FirebaseAuth.getInstance();
+
 
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +62,55 @@ public class Login extends AppCompatActivity {
                 Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtUser.getText().toString();
+                String password = txtPass.getText().toString();
+
+                layoutEmail.setError(null);
+                layoutPass.setError(null);
+
+                if (email.isEmpty() && password.isEmpty()) {
+                    layoutEmail.setError("Email đang để trống");
+                    layoutPass.setError("Mật khẩu đang để trống");
+                    return;
+                }
+
+                if (email.isEmpty()) {
+                    layoutEmail.setError("Email đang để trống");
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    layoutPass.setError("Mật khẩu đang để trống");
+                    return;
+                }
+
+                btndangnhap.setEnabled(false);
+                btndangnhap.setText("Đang xử lý...");
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this, "Đăng nhập thành công.", Toast.LENGTH_SHORT).show();
+//                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                                    startActivity(intent);
+//                                    finish();
+                                } else {
+                                    Toast.makeText(Login.this, "Tài khoản hoặc mật khẩu sai.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                btndangnhap.setEnabled(true);
+                                btndangnhap.setText("Login");
+                            }
+                        });
             }
         });
     }
